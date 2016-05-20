9100a40dae7c54ccda235606d326741e8d4e24b4
Katelyn Rodrigue <katelynrodrigue@Katelyns-MacBook-Pro.local>
2016-05-19 21:35:40 -0400

    make work

--- bugCalc.js
+++ bugCalc.js
@@ -1,26 +1,20 @@
 var main = function(){
 
-	var output = function(text){
-		$("#calculatorOutput").text(text);
-	}
 	$("#ladybug").click(function(){
-		$(this).removeClass("")
-		$(this).addClass("black");
-
-		output("But did you ask Keith");
+		$("#calculatorOutput").text("but did you ask Keith");
 	})
 	$("#android").click(function(){
 		$("#calculatorOutput").text("I think that's just how that works on Android");
 
 	})
 	$("#crash").click(function(){
-		$("#calculatorOutput").text("But can you reproduce it");
+		$("#calculatorOutput").text("but can you reproduce it");
 	})
 	$("#usability").click(function(){
-		$("#calculatorOutput").text("Haven't heard about it from CS");
+		$("#calculatorOutput").text("haven't heard about it from CS");
 	})
 	$("#freeze").click(function(){
-		$("#calculatorOutput").text("Are you sure it's not like that in production");
+		$("#calculatorOutput").text("are you sure it's not like that in production");
 	})
 	$("#memory").click(function(){
 		$("#calculatorOutput").text("I didn't even touch that");
@@ -29,10 +23,10 @@ var main = function(){
 		$("#calculatorOutput").text("I think it's supposed to look like that");
 	})
 	$("#error").click(function(){
-		$("#calculatorOutput").text("Well it doesn't let them save like that so");
+		$("#calculatorOutput").text("well it doesn't let them save like that so");
 	})
 	$("#apple").click(function(){
-		$("#calculatorOutput").text("That's actually expected behavior");
+		$("#calculatorOutput").text("that's actually expected behavior");
 	})
 	
 }
